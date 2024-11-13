d7e96d41f7ebef0ee4bde38ea451323324302b3e
Ulyana Kuznetsova <rey.tsvetkov777@gmail.com>
2024-11-13 23:51:19 +0300

    adit readme

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,22 @@
-from typing import List, Dict, Optional
+from typing import List, Dict
+from datetime import datetime
 
-def filter_by_state(data: List[Dict], state: Optional[str] = 'EXECUTED') -> List[Dict]:
-    """Фильтрует список транзакций по заданному состоянию (по умолчанию 'EXECUTED')."""
-    return [item for item in data if item.get('state') == state]
+def filter_by_state(transactions: List[Dict[str, str]], state: str = 'EXECUTED') -> List[Dict[str, str]]:
+    """
+    Фильтрует список транзакций по статусу.
 
-def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
-    """Сортирует список транзакций по дате (по умолчанию в порядке убывания)."""
-    return sorted(data, key=lambda x: x['date'], reverse=descending)
+    :param transactions: список словарей с данными транзакций
+    :param state: статус для фильтрации (по умолчанию 'EXECUTED')
+    :return: отфильтрованный список транзакций
+    """
+    return [transaction for transaction in transactions if transaction.get('state') == state]
+
+def sort_by_date(transactions: List[Dict[str, str]], descending: bool = True) -> List[Dict[str, str]]:
+    """
+    Сортирует список транзакций по дате.
+
+    :param transactions: список словарей с данными транзакций
+    :param descending: порядок сортировки (по умолчанию убывающий)
+    :return: отсортированный список транзакций
+    """
+    return sorted(transactions, key=lambda x: datetime.fromisoformat(x['date']), reverse=descending)
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,9 @@
-def mask_account_card(card_info):
+from datetime import datetime  # Переместите импорт сюда
+
+def mask_account_card(card_info: str) -> str:
+    """
+    Маскирует номер карты или счета, оставляя видимыми последние 4 цифры.
+    """
     # Разделяем строку на тип карты/счета и номер
     type_number = card_info.split(" ")[1]
     # Проверяем, является ли первая цифра нулем (если да, то это счет)
@@ -7,8 +12,10 @@ def mask_account_card(card_info):
     else:
         return f"{card_info[:12]} {card_info[12:-4]} **** {card_info[-4:]}"
 
-    import datetime
+def get_date(dt_str: str) -> str:
+    """
+    Преобразует строку даты-времени в формат dd.mm.yyyy.
+    """
+    dt_obj = datetime.strptime(dt_str, '%Y-%m-%dT%H:%M:%S.%f')
+    return dt_obj.strftime('%d.%m.%Y')
 
-    def get_date(dt_str):
-        dt_obj = datetime.datetime.strptime(dt_str, '%Y-%m-%dT%H:%M:%S.%f')
-        return dt_obj.strftime('%d.%m.%Y')
